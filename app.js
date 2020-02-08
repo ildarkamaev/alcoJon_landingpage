5400559d0e2459bce88e3d25c181bbe5c682c56b
ildar <hilnur5@gmail.com>
2020-02-08 19:13:09 +0200

    added jump js

---
+++ app.js
@@ -0,0 +1,8 @@
+// import jump from "jump.js";
+let aboutLink = document.querySelector('.aboutLink');
+let howLink = document.querySelector('.howLink');
+let contactLink = document.querySelector('.contactLink');
+
+aboutLink.addEventListener('click', ()=>{
+    jump('#about');
+})
